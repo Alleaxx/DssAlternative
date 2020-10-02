5811f1800486da64359cfae407841a8124c94dc9
Alleaxx <32822043+Alleaxx@users.noreply.github.com>
2020-10-02 15:28:03 +0300

    Добавьте файлы проекта.

---
+++ DSS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DSSConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DSSConsole");
+            Console.WriteLine("Начало разработки системы поддержки принятия решения по критериям");
+            Console.WriteLine("Тсс, никто этого не увидит!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DSSLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSSLib
+{
+    public class Class1
+    {
+    }
+    public class TEst
+    {
+
+    }
+}
